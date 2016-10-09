1a0885c290e28340857b57d19dd11c4f732203ec
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-09 22:49:38 +1100

    Setting up React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 import './css/style.css';
 
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
 ReactDOM.render(
-  <Main />,
+  router,
   document.getElementById('root')
 );
