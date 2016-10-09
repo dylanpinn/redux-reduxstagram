db5c7e59ea2cb50afee42b99c3c221c3ba9919c0
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-09 22:23:03 +1100

    App Layout + Component Setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './components/Main';
 import './css/style.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <p>Hi</p>,
+  <Main />,
   document.getElementById('root')
 );
