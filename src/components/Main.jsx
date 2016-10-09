cf20ab7a12303601a1b9d7bc62f73f748b1f1c36
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-09 22:35:55 +1100

    Creating our Single and PhotoGrid components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Main extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.any,
+  }
+
   render() {
     return (
       <div>
         <h1>
           <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     );
   }
