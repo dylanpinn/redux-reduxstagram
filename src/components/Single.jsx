9e10280c8bfa0515696dede606394c8f295f2a36
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-10 21:35:56 +1100

    Displaying the Single Photo Component

--- src/components/Single.jsx
+++ src/components/Single.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
+  static propTypes = {
+    posts: React.PropTypes.array,
+    params: React.PropTypes.object,
+  }
+
   render() {
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-        Single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     );
   }
