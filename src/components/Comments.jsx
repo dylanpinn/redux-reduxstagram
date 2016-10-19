61f36782201d2bf424340d1dcb2d1ee1c5285c9b
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-19 23:50:40 +1100

    More types

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -5,26 +5,26 @@ import type { Comment } from '../types';
 type Props = {
   postComments: Comment[],
   params: { postId: string },
-  removeComment: Function,
-  addComment: Function,
+  removeComment: (postId: string, i: number) => Object,
+  addComment: (postId: string, author: string, comment: string) => Object,
 }
 
 class Comments extends React.Component {
   props: Props;
-  author: any;
-  comment: any;
-  commentForm: any;
+  author: HTMLInputElement;
+  comment: HTMLInputElement;
+  commentForm: HTMLFormElement;
 
   handleSubmit = (e: Event): void => {
     e.preventDefault();
     const { postId } = this.props.params;
-    const author: mixed = this.author.value;
-    const comment: mixed = this.comment.value;
+    const author: string = this.author.value;
+    const comment: string = this.comment.value;
     this.props.addComment(postId, author, comment);
     this.commentForm.reset();
   };
 
-  renderComment = (comment: Object, i: number): any => (
+  renderComment = (comment: Comment, i: number): any => (
     <div className="comment" key={i}>
       <p>
         <strong>{comment.user}</strong>
