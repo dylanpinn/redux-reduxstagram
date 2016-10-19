5a43f458ffa2788661b297e8297f414490572702
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-19 20:50:16 +1100

    Clean up Single

--- src/components/Single.jsx
+++ src/components/Single.jsx
@@ -3,29 +3,24 @@ import React from 'react';
 import Photo from './Photo';
 import Comments from './Comments';
 
-class Single extends React.Component {
-  static propTypes = {
-    posts: React.PropTypes.arrayOf(React.PropTypes.object),
-    params: React.PropTypes.shape({
-      postId: React.PropTypes.string,
-    }),
-    comments: React.PropTypes.object,
-  }
-
-  render() {
-    const { postId } = this.props.params;
-    const { posts, comments } = this.props;
-    const i = posts.findIndex(post => post.code === postId);
-    const post = posts[i];
-    const postComments = comments[postId] || [];
-
-    return (
-      <div className="single-photo">
-        <Photo i={i} post={post} {...this.props} />
-        <Comments postComments={postComments} {...this.props} />
-      </div>
-    );
-  }
+type Props = {
+  posts: Object[],
+  params: { postId: string },
+  comments: Object,
 }
+
+const Single = ({ posts, params, comments, ...props }: Props) => {
+  const { postId } = params;
+  const i = posts.findIndex(post => post.code === postId);
+  const post = posts[i];
+  const postComments = comments[postId] || [];
+
+  return (
+    <div className="single-photo">
+      <Photo i={i} post={post} {...props} />
+      <Comments postComments={postComments} {...props} />
+    </div>
+  );
+};
 
 export default Single;
