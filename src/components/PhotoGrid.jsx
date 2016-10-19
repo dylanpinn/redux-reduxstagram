3880a3b55e1afd32c86e052e1a7f7e42e7735dae
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-19 21:08:52 +1100

    Convert PhotoGrid to stateless component

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import Photo from './Photo';
 
-class PhotoGrid extends React.Component {
-  render() {
-    return (
-      <div className="photo-grid">
-        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
-      </div>
-    );
-  }
+type Props = {
+  posts: Object[],
 }
+
+const PhotoGrid = ({ posts, ...props }: Props) => (
+  <div className="photo-grid">
+    {posts.map((post, i) => <Photo {...props} key={i} i={i} post={post} />)}
+  </div>
+);
 
 export default PhotoGrid;
