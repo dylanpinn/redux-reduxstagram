1e21c3f185994b87d793ed743c6d9d145b3bb30b
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-19 21:06:00 +1100

    Convert Photo to stateless component

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -3,45 +3,40 @@ import React from 'react';
 import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
-class Photo extends React.Component {
-  static propTypes = {
-    post: React.PropTypes.object.isRequired,
-    i: React.PropTypes.number.isRequired,
-    comments: React.PropTypes.object,
-    increment: React.PropTypes.func.isRequired,
-  }
-
-  render() {
-    const { post, i, comments } = this.props;
-    return (
-      <figure className="grid-figure">
-        <div className="grid-photo-wrap">
-          <Link to={`/view/${post.code}`}>
-            <img src={post.display_src} alt={post.caption} className="grid-photo" />
-          </Link>
-          <CSSTransitionGroup
-            transitionName="like"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}
-          >
-            <span className="likes-heart" key={post.likes}>{post.likes}</span>
-          </CSSTransitionGroup>
-        </div>
-        <figcaption>
-          <p>{post.caption}</p>
-          <div className="control-buttons">
-            <button onClick={() => this.props.increment(i)} className="likes">&hearts; {post.likes}</button>
-            <Link className="button" to={`/view/${post.code}`}>
-              <span className="comment-count">
-                <span className="speech-bubble"></span>
-                {comments[post.code] ? comments[post.code].length : 0}
-              </span>
-            </Link>
-          </div>
-        </figcaption>
-      </figure>
-    );
-  }
+type Props = {
+  post: Object,
+  i: number,
+  comments: ?Object,
+  increment: Function,
 }
+
+const Photo = ({ post, i, comments, ...props }: Props) => (
+  <figure className="grid-figure">
+    <div className="grid-photo-wrap">
+      <Link to={`/view/${post.code}`}>
+        <img src={post.display_src} alt={post.caption} className="grid-photo" />
+      </Link>
+      <CSSTransitionGroup
+        transitionName="like"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+      >
+        <span className="likes-heart" key={post.likes}>{post.likes}</span>
+      </CSSTransitionGroup>
+    </div>
+    <figcaption>
+      <p>{post.caption}</p>
+      <div className="control-buttons">
+        <button onClick={() => props.increment(i)} className="likes">&hearts; {post.likes}</button>
+        <Link className="button" to={`/view/${post.code}`}>
+          <span className="comment-count">
+            <span className="speech-bubble" />
+            {comments[post.code] ? comments[post.code].length : 0}
+          </span>
+        </Link>
+      </div>
+    </figcaption>
+  </figure>
+);
 
 export default Photo;
